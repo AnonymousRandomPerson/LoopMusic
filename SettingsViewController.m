b779004a2bfe84fa72a0ab5ea0c2a1502b748628
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2015-10-11 21:23:01 -0500

    Fixed updating URL issue

--- SettingsViewController.m
+++ SettingsViewController.m
@@ -279,15 +279,7 @@
         [self prepareQuery:[NSString stringWithFormat:@"SELECT url FROM Tracks WHERE name=\"%@\"", itemName]];
         if (sqlite3_step(statement) == SQLITE_ROW)
         {
-            NSString *urlString = @"";
-            if (sqlite3_column_text(statement, 0) != nil)
-            {
-                urlString = [[NSString alloc] initWithUTF8String:(const char *) sqlite3_column_text(statement, 0)];
-            }
-            if ([urlString isEqualToString:@""])
-            {
-                [self updateDB:[NSString stringWithFormat:@"UPDATE Tracks SET url = \"%@\" WHERE name = \"%@\"", itemURL.absoluteString, itemName]];
-            }
+            [self updateDB:[NSString stringWithFormat:@"UPDATE Tracks SET url = \"%@\" WHERE name = \"%@\"", itemURL.absoluteString, itemName]];
         }
         else
         {
